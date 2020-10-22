1f1e178daaafb629336c77253fe46e8a8bc9a34a
Aayush Maharjan <mhrzn.aayush@gmail.com>
2020-10-22 17:40:51 +0545

    New and imporved

--- Dice.py
+++ Dice.py
@@ -1,15 +1,48 @@
-
 import cv2
 import numpy as np
 
-# convert image to canny and find contours
-# count the number of contours whoes area is greater than 200
+# divies the images into different pars according to identified frames
+# returns lists of x-coordinate, y-coordinate , width and height of each frame
 
 
-def predictDieNumber(img):
-    imgCanny = cv2.Canny(img, 600, 600)
+def getDie(img):
+
+    contours, hierarchy = cv2.findContours(
+        img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    objNum = len(contours)
+    x, y, width, height = [], [], [], []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+
+        if area > 200:
+
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            a, b, w, h = cv2.boundingRect(approx)
+            x.append(a)
+            y.append(b)
+            width.append(w)
+            height.append(h)
+            cv2.putText(imgContour, "Dots:", (a+(w//2)-10, b+h-4),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
+            cv2.rectangle(imgContour, (a, b),
+                          (a+w, b+h), (0, 255, 0), 3)
+
+            # cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 2)
+            # cv2.imshow("Contour", imgContour)
+
+    return x, y, width, height, objNum
+
+# calculates the number of countours in each image
+
+
+def predictDie(img):
+    imgCanny = cv2.Canny(img, 50, 50)
+
     contours, hierarchy = cv2.findContours(
         imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # cv2.imshow("img101", imgCanny)
     n = 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
@@ -19,41 +52,39 @@ def predictDieNumber(img):
     return n
 
 
-img = cv2.imread("./img.jpg")
+originalImage = cv2.imread("./inp1.png")
+oheight = originalImage.shape[0]
+owidth = originalImage.shape[1]
+
+# increasing the height of the image because divided image is to small for a normal frame
+img = cv2.resize(originalImage, (500, 500))
+
+
+imgContour = img.copy()
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+imgCanny = cv2.Canny(imgBlur, 50, 50)
+
+
+# cv2.imshow("Grey", imgGray)
+# cv2.imshow("Blur", imgBlur)
+# cv2.imshow("Canny", imgCanny)
+x, y, width, height, objnum = getDie(imgCanny)
 
 # crop the image into number of dice
-die1 = img[0:130, 0:170]
-die2 = img[0:130, 170:]
-die3 = img[130:, :115]
-die4 = img[130:, 114:230]
-die5 = img[130:, 220:]
+die = []
+for i in range(objnum):
 
-# imgContour = die1.copy()
+    die.append(img[y[i]: y[i] + height[i], x[i]: x[i] + width[i]])
+    # cv2.imshow("die"+str(i), die[i])
 
-# get the number of contours in the image
-dienum1 = predictDieNumber(die1)
-dienum2 = predictDieNumber(die2)
-dienum3 = predictDieNumber(die3)
-dienum4 = predictDieNumber(die4)
-dienum5 = predictDieNumber(die5)
-
-# put text in the image
-cv2.putText(img, "Dots:" + str(dienum1), (80, 130),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
-cv2.putText(img, "Dots:" + str(dienum2), (200, 130),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
-cv2.putText(img, "Dots:" + str(dienum3), (30, 260),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
-cv2.putText(img, "Dots:" + str(dienum4), (140, 260),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
-cv2.putText(img, "Dots:" + str(dienum5), (250, 260),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
-
-# show image
-cv2.imshow("img", img)
+    n = predictDie(die[i])
+    cv2.putText(img, "Dots:"+str(n), (x[i]+(width[i]//2)-50, y[i]+height[i]-4),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+imgOriginal = cv2.resize(img, (owidth, oheight))
+cv2.imshow("Original", imgOriginal)
 
 # write image to a file
-cv2.imwrite('labelledDice.jpg', img)
+cv2.imwrite('labelledDice.png', imgOriginal)
 
 cv2.waitKey(0)
-cv2.destroyAllWindows()
